1a4afdaf304b30ae161f6d94f80c5627c5b4b70f
Açal Habi <habi_a@etna-alternance.net>
2020-02-19 22:23:30 +0100

    size player + skin

--- sdl/main.c
+++ sdl/main.c
@@ -11,13 +11,13 @@
 
 int main()
 {
-    const int tile_size = 32;
+    const int tile_size = 38;
     game_t *game = game_init(25 * tile_size, 21 * tile_size, tile_size, 2);
 
     game_create_window(game);
     game->map = map_load(game->renderer, game->tile_size, "./map.txt");
     game->player1 = player_load(game->renderer, 1, 1, game->tile_size, 
-                                                        "./mario.png");
+                                                        "./player.png");
     game->player1->bag = bag_load(game->renderer, game->tile_size, 
                                     game->max_bombs, "./bomb.png");
     if (game->player1 == NULL || game->player1->bag == NULL) {
